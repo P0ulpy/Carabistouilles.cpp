259616a72b5d69b03d9bb5d6eae26d5af0c0e878
Florian <47593350+P0ulpy@users.noreply.github.com>
2023-11-03 16:42:16 +0100

    Some cleaning

--- vcpkg_test/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Flo/Documents/_Code/Carabistouilles.cpp/vcpkg_test
-# Build directory: C:/Users/Flo/Documents/_Code/Carabistouilles.cpp/vcpkg_test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
